34c5023f096ed2826bdf4c173e364a4018f4010a
RayaanRA <rayaanahmad316@gmail.com>
2026-01-09 19:05:28 -0600

    add grav_force_body()

--- src/force.c
+++ src/force.c
@@ -3,6 +3,15 @@
 void grav_force_body(phys_body* body, phys_system* system) {
 	for (int i = 0; i < system->count; i++) {
 		if (&(system->bodies[i]) == body) {continue;}
+
 		float dist = phys_vector2_dist(body->pos, (system->bodies[i]).pos);
+		if (dist == 0.0f) {continue;}
+		phys_vector2 displacement = phys_vector2_sub(body->pos, (system->bodies[i]).pos);
+
+		float inv_dist = 1.0f / dist;
+		float scalar_factor = -G * body->mass * (system->bodies[i]).mass * (inv_dist * inv_dist * inv_dist);
+		
+		phys_vector2 force = phys_scalar_mult(scalar_factor, displacement);
+		body->force = phys_vector2_add(body->force, force);
 	}
 }
